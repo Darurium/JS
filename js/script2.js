746773f44b69c14b03778db4d3d1f0f3e22e64f5
Darurium <darurium@gmail.com>
2020-11-20 16:09:24 +0500

    object_practic

--- js/script2.js
+++ js/script2.js
@@ -1,21 +1,21 @@
-'use strict';
+// 'use strict';
 
-const soldier = {
-    healf: 400,
-    armor: 100,
-    sayHello: function() {
-        console.log('Hello');
-    }
-};
-
-// const jonh = {
-//     healf: 100
+// const soldier = {
+//     healf: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log('Hello');
+//     }
 // };
 
-const jonh = Object.create(soldier);
+// // const jonh = {
+// //     healf: 100
+// // };
 
-// jonh.__proto__ = soldier;
+// const jonh = Object.create(soldier);
 
-Object.setPrototypeOf(jonh, soldier);
+// // jonh.__proto__ = soldier;
 
-jonh.sayHello();
+// Object.setPrototypeOf(jonh, soldier);
+
+// jonh.sayHello();
